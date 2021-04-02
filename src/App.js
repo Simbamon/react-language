ef0569631ab3d6a304d41dfb49616445309f6c6d
Simbamon <sbhan9511@gmail.com>
2021-04-02 23:41:23 +0900

    Added translation components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+  
+  function handleClick(lang){
+    i18n.changeLanguage(lang);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <p onClick={()=>handleClick('en')}>English</p>
+        <p onClick={()=>handleClick('ko')}>Korean</p>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{t('Language.1')}</p>
+        <p>{t('Summary.1')}</p>
+
       </header>
     </div>
   );
